a70f6943a9d8f63c19357fbf253cf82b4fcdb887
minotaa <jpichardo369@gmail.com>
2024-12-05 14:31:20 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="md:container pl-8 pt-8 pr-4">
+  <h1 class="font-dungeon text-6xl text-white">Mirkwood</h1>
+  <p class="font-sink text-lg text-white">An autobattler RPG mobile game created by <a target="_blank" href="https://minota.cc" class="transition-colors text-sky-500 hover:text-sky-600">Juan Pichardo</a>.</p>
+
+  <h2 class="font-dungeon text-5xl text-white mt-6 mb-4">Important Links</h2>
+  <div class="flex md:flex-row flex-col md:gap-4 gap-2">
+    <a target="_blank" href="https://minota.cc" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">App Store</a>
+    <a target="_blank" href="/privacy" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Privacy Policy</a>
+    <a target="_blank" href="https://discord.gg/MZhUHZnt6y" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Support Server</a>
+  
+  </div>
+</div>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,33 @@
+<div class="container pl-4 md:mx-auto flex justify-items-center mx-auto flex-col pt-4 font-sink mb-8">
+  <h1 class="text-6xl text-white font-dungeon">Contact Me</h1>
+  <p class="text-lg text-white mt-4">
+    Contact me on <a class="text-sky-500" href="https://discord.gg/MZhUHZnt6y">this Discord</a> or by 
+    <a class="text-sky-500" href="mailto:me@minota.cc">email</a>.
+  </p>
+  
+  <h1 class="text-6xl text-white font-dungeon mt-8">Privacy Policy</h1>
+  <p class="text-lg text-white mt-4">
+    Mirkwood ("we," "our," or "us") respects your privacy and is committed to protecting any information you may provide while using our app. This Privacy Policy explains how we handle your data, including the use of AdMob ads in our app.
+    <br><br>
+    1. Data Collection<br>
+    Our app uses AdMob to serve ads. AdMob may collect certain information automatically, such as device identifiers, IP addresses, and data about your interactions with ads, to personalize and improve the ad experience. For more information, refer to 
+    <a class="text-sky-500" href="https://policies.google.com/privacy">Google's Privacy Policy</a>.
+    <br><br>
+    2. Data Use<br>
+    Data collected by AdMob is used to provide personalized ads and improve the relevance of advertising. We do not directly collect or store any personal data.
+    <br><br>
+    3. Third-Party Services<br>
+    By using our app, you agree to the use of third-party services like AdMob, which may collect and process your data. You can opt out of personalized ads through the device settings or by visiting 
+    <a class="text-sky-500" href="https://www.google.com/settings/ads">Google Ads Settings</a>.
+    <br><br>
+    4. Children’s Privacy<br>
+    Mirkwood is safe for all ages. However, if you are a parent or guardian and believe your child’s information has been inadvertently collected, please contact us immediately.
+    <br><br>
+    5. Changes to This Privacy Policy<br>
+    We may update our Privacy Policy from time to time. Any changes will be posted within the app, and your continued use of the app after any updates means you accept the revised policy.
+    <br><br>
+    6. Contact Us<br>
+    If you have any questions about this Privacy Policy, please contact us at 
+    <a class="text-sky-500" href="mailto:me@minota.cc">this email</a>.
+  </p>
+</div>
