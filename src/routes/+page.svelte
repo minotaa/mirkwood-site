a114aa12d17023b6b678d36090a4f38a254ca616
minotaa <jpichardo369@gmail.com>
2024-12-07 08:24:42 -0500

    add app store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
   <h2 class="font-dungeon text-5xl text-white mt-6 mb-4">Important Links</h2>
   <div class="flex md:flex-row flex-col md:gap-4 gap-2">
-    <a target="_blank" href="https://minota.cc" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">App Store</a>
+    <a target="_blank" href="https://apps.apple.com/us/app/mirkwood/id6738539616" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">App Store</a>
     <a target="_blank" href="/privacy" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Privacy Policy</a>
     <a target="_blank" href="https://discord.gg/MZhUHZnt6y" class="border-8 border-white pt-4 pb-4 pl-8 pr-8 text-xl font-sink text-white hover:bg-zinc-800 transition-colors">Support Server</a>
   
