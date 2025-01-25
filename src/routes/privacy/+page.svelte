818a4eaf1ce018572ee0e128e3a7875639883e36
minota <34592028+minotaa@users.noreply.github.com>
2025-01-25 07:22:24 -0500

    Update +page.svelte

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -7,7 +7,7 @@
   
   <h1 class="text-6xl text-white font-dungeon mt-8">Privacy Policy</h1>
   <p class="text-lg text-white mt-4">
-    Mirkwood ("we," "our," or "us") respects your privacy and is committed to protecting any information you may provide while using our app. This Privacy Policy explains how we handle your data, including the use of AdMob ads in our app.
+    Myrkwood ("we," "our," or "us") respects your privacy and is committed to protecting any information you may provide while using our app. This Privacy Policy explains how we handle your data, including the use of AdMob ads in our app.
     <br><br>
     1. Data Collection<br>
     Our app uses AdMob to serve ads. AdMob may collect certain information automatically, such as device identifiers, IP addresses, and data about your interactions with ads, to personalize and improve the ad experience. For more information, refer to 
@@ -21,7 +21,7 @@
     <a class="text-sky-500" href="https://www.google.com/settings/ads">Google Ads Settings</a>.
     <br><br>
     4. Children’s Privacy<br>
-    Mirkwood is safe for all ages. However, if you are a parent or guardian and believe your child’s information has been inadvertently collected, please contact us immediately.
+    Myrkwood is safe for all ages. However, if you are a parent or guardian and believe your child’s information has been inadvertently collected, please contact us immediately.
     <br><br>
     5. Changes to This Privacy Policy<br>
     We may update our Privacy Policy from time to time. Any changes will be posted within the app, and your continued use of the app after any updates means you accept the revised policy.
